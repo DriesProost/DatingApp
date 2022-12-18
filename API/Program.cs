bfd8f7d6cf5d5f47949fdb2c37fffff3f3dc4604
Dries Proost <driesproost@hotmail.com>
2022-12-18 15:40:15 +0100

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -14,6 +14,8 @@ opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+
 app.MapControllers();
 
 app.Run();
